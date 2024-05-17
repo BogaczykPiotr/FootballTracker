afe48abc1cbba72d7e151766c8c012818ed5b9b3
Bajoo7 <bartosz.konior7@gmail.com>
2024-05-17 21:56:15 +0200

    last

---
+++ FootballTracker/FootballTracker/MatchesPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FootballTracker
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MatchesPage : ContentPage
+    {
+        public MatchesPage()
+        {
+            InitializeComponent();
+            var swipeGesture = new SwipeGestureRecognizer();
+            swipeGesture.Swiped += OnSwiped;
+            DateSlider.GestureRecognizers.Add(swipeGesture);
+        }
+
+        void OnSwiped(object sender, SwipedEventArgs e)
+        {
+            if (e.Direction == SwipeDirection.Left)
+            {
+                // Przesuń slider w lewo
+                DateSlider.TranslateTo(-100, 0, 500, Easing.Linear);
+            }
+            else if (e.Direction == SwipeDirection.Right)
+            {
+                // Przesuń slider w prawo
+                DateSlider.TranslateTo(100, 0, 500, Easing.Linear);
+            }
+        }
+    }
+}
